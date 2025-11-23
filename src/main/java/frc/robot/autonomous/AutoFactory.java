ece27971f7e92b4a87fb952b2eea7ac02996c87c
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-11-22 19:17:19 -0800

    does this work?

--- src/main/java/frc/robot/autonomous/AutoFactory.java
+++ src/main/java/frc/robot/autonomous/AutoFactory.java
@@ -127,6 +127,8 @@ public class AutoFactory {
         return Pair.of(
             FieldConstants.START,
             Commands.defer(() -> Commands.sequence(
+                drivetrain.getAutonomousCommand("shooter_1"),
+                drivetrain.trackAprilTag(),
                 drivetrain.getAutonomousCommand("shooter_2"),
                 drivetrain.trackAprilTag(),
                 drivetrain.getAutonomousCommand("shooter_3"),
