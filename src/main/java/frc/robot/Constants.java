bcbfdc063e61d4289a82355d6780e3300c633100
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-12-02 18:38:57 -0800

    constants yay

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
       "limelight-right",
     };
 
-    public static final double kStdvXYBase = 0.03; // No idea how to tune these base values.
+    public static final double kStdvXYBase = 0.3; // No idea how to tune these base values.
     public static final double kStdvThetaBase = 1.0; // See above
     public static final double JUMP_TOLERANCE = 0.5; // meters. again, needs tuning.
   }
