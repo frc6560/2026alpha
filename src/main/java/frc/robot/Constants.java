8a139a7d595d16c4a75d361d3f92fe7391857302
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-12-07 22:21:55 -0800

    build fix ur stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,9 +76,9 @@ public final class Constants {
       switch(name){
         case "limelight":
           limelightPose = new Pose3d(
-            0.192,
-            -0.394,
             0.0248,
+            -0.394,
+            0.192,
             new Rotation3d(0, 40, 90)
           );
           break;
